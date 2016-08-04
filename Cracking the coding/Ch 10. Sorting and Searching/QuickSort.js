5afa60a4e74c7f490259faefd81382cb39597159
Mingchao Zou <zouc@sunyit.edu.com>
2016-08-04 10:27:24 -0400

    :watermelon:  Implement Quick Sort

---
+++ Cracking the coding/Ch 10. Sorting and Searching/QuickSort.js
@@ -0,0 +1,73 @@
+/*
+* Quick Sort with unique elements
+* Time / Space Complexity: O(NlogN) / O(1)
+* Author: Mingchao Zou, Aug 4
+*/
+
+function quickSort(arr, low, high){
+
+    function swap(i, j) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    if(low >= high){
+        return;
+    }
+
+    var position;    
+    var pivot = arr[low];//assume the first element as the pivot
+    var left = low + 1, right = high;
+
+    while(left < right){
+        while(left < right && arr[left] < pivot){
+            left++;
+        }
+
+        while(left < right && arr[right] > pivot){
+            right--;
+        }
+        
+        if(left < right){
+            swap(left, right);
+        }
+    }
+
+    if(left == right){ //swap the pivot with the rightmost element less than it.
+        if(arr[left] > pivot){
+            swap(low, left - 1);
+            position = left - 1;
+        }else{
+            swap(low, left);
+            position = left;
+        }
+    }
+
+    quickSort(arr, low, position - 1);
+    quickSort(arr, position + 1, high);
+}
+
+function print(arr){
+    arr.forEach(function(element){
+        console.log(element);
+    });
+}
+
+var array = [2, 1, 8, 9, 5, 4, 0, 3];
+
+quickSort(array, 0, array.length - 1);
+
+print(array);
+
+/*
+* Result:
+    0
+    1
+    2
+    3
+    4
+    5
+    8
+    9
+*/
