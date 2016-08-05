d181d6e4007e5a2a199b620a9bc1172dd6b89fc8
Mingchao Zou <zouc@sunyit.edu.com>
2016-08-04 22:06:00 -0400

    Add iterative binary search

--- Cracking the coding/Ch 10. Sorting and Searching/BinarySearch.js
+++ Cracking the coding/Ch 10. Sorting and Searching/BinarySearch.js
@@ -13,7 +13,7 @@ function binarySearch(arr, val) {
 
         var mid = Math.floor((low + high) / 2);
         if(arr[mid] == val){
-            return val;
+            return mid;
         }else if(arr[mid] > val){
             return binarySearchRecursive(low, mid - 1);
         }else{
@@ -22,6 +22,24 @@ function binarySearch(arr, val) {
     }
 
     return binarySearchRecursive(0, arr.length - 1);
+}
+
+function binarySearch2(arr, val) {
+    var low = 0;
+    var high = arr.length - 1;
+
+    while(low <= high){
+        var mid = Math.floor((low + high) / 2);
+        if(arr[mid] == val){
+            return mid;
+        }else if(arr[mid] > val){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+
+    return Number.NEGATIVE_INFINITY;
 }
 
 function print(arr){
@@ -34,6 +52,31 @@ var array = [2, 4, 8, 9, 13, 14, 20, 333];
 
 print(array);
 
-console.log("Found: " + binarySearch(array, 20));
-console.log("Found: " + binarySearch(array, 120));
-console.log("Found: " + binarySearch(array, 4));
+console.log("Recursive Found at: " + binarySearch(array, 20));
+console.log("Recursive Found at: " + binarySearch(array, 120));
+console.log("Recursive Found at: " + binarySearch(array, 4));
+
+
+console.log("Iterative Found at: " + binarySearch2(array, 20));
+console.log("Iterative Found at: " + binarySearch2(array, 120));
+console.log("Iterative Found at: " + binarySearch2(array, 4));
+
+/*
+* Result:
+    2
+    4
+    8
+    9
+    13
+    14
+    20
+    333
+
+    Recursive Found at: 6
+    Recursive Found at: -Infinity
+    Recursive Found at: 1
+    
+    Iterative Found at: 6
+    Iterative Found at: -Infinity
+    Iterative Found at: 1
+*/
