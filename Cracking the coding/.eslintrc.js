fe60285db1f7175690ce76ba895b156735425777
Mingchao Zou <zouc@sunyit.edu.com>
2016-11-10 11:21:15 -0500

    Set up unit testing environment for javascript

---
+++ Cracking the coding/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "jasmine": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ Cracking the coding/karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config){
+    config.set({
+        frameworks: ['browserify', 'jasmine'],
+        files: [
+            'Ch 08. Recursion and Dynamic Programming/src/**/*.js',
+            'Ch 08. Recursion and Dynamic Programming/test/**/*_spec.js'
+        ],
+        preprocessors:{
+            '**/test/**/*.js': ['browserify'],
+            '**/src/**/*.js': ['browserify']
+        },
+        browsers: ['Chrome'],
+        browserify: {
+            debug: true
+        }
+    });
+}
