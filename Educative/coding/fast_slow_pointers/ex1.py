b3edc5b176348dd2ed7ce8cd0502b876d8c89ce3
Kevin Zou <kevinzou19@gmail.com>
2020-10-14 20:40:34 -0400

    rename dir naem

--- Educative/coding/fast_slow_pointers/ex1.py
+++ Educative/coding/fast_slow_pointers/ex1.py
@@ -6,9 +6,12 @@ class Node:
 
 def has_cycle(head):
     slow, fast = head, head
+
     while fast is not None and fast.next is not None:
         fast = fast.next.next
         slow = slow.next
+
         if slow == fast:
             return True  # found the cycle
+
     return False
